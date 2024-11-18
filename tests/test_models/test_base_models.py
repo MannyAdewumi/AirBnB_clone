037920a859ee731218625901c50445b61da57454
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-11-18 02:01:03 -0800

    airbnb clone

--- tests/test_models/test_base_models.py
+++ tests/test_models/test_base_models.py
@@ -7,8 +7,7 @@ import unittest
 from models.base_model import BaseModel
 
 
-
-class TestBasemodel(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """
     Unittest for BaseModel
     """
@@ -34,12 +33,12 @@ class TestBasemodel(unittest.TestCase):
             os.rename("tmp.json", "file.json")
         except FileNotFoundError:
             pass
+
     def test_init(self):
         """
         Test for init
         """
         my_model = BaseModel()
-
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
         self.assertIsNotNone(my_model.updated_at)
@@ -49,11 +48,8 @@ class TestBasemodel(unittest.TestCase):
         Test for save method
         """
         my_model = BaseModel()
-
         initial_updated_at = my_model.updated_at
-
         current_updated_at = my_model.save()
-
         self.assertNotEqual(initial_updated_at, current_updated_at)
 
     def test_to_dict(self):
@@ -61,27 +57,24 @@ class TestBasemodel(unittest.TestCase):
         Test for to_dict method
         """
         my_model = BaseModel()
-
         my_model_dict = my_model.to_dict()
-
         self.assertIsInstance(my_model_dict, dict)
-
         self.assertEqual(my_model_dict["__class__"], 'BaseModel')
         self.assertEqual(my_model_dict['id'], my_model.id)
-        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
-        self.assertEqual(my_model_dict["updated_at"], my_model.created_at.isoformat())
-
+        self.assertEqual(
+            my_model_dict['created_at'], my_model.created_at.isoformat()
+        )
+        self.assertEqual(
+            my_model_dict["updated_at"], my_model.updated_at.isoformat()
+        )
 
     def test_str(self):
         """
         Test for string representation
         """
         my_model = BaseModel()
-
         self.assertTrue(str(my_model).startswith('[BaseModel]'))
-
         self.assertIn(my_model.id, str(my_model))
-
         self.assertIn(str(my_model.__dict__), str(my_model))
 
 
